4cdf166c2264d52b9a282afbf436f1eca7b9a003
HackkcaH <37765695+HackkcaH@users.noreply.github.com>
2020-12-17 04:08:34 -0300

    remoção lixo

--- php/pass.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (strpos($_SERVER['PHP_SELF'], "/var/www/") === 0) { }else{
-  if (basename($_SERVER["REQUEST_URI"]) === basename(__FILE__)){ header('Location: /404'); }
-}
-$pass = '123';
-$pass1 = '456';
-?>
